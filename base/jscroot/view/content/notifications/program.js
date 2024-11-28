19727611f8a5684e71b76bb611a01c2ac300e2ad
Dewi Kresnawati <dewidesember20@gmail.com>
2024-11-28 16:45:41 +0700

    pembaruan struktur

--- base/jscroot/view/content/notifications/program.js
+++
@@ -1,5 +0,0 @@
-import {setInner} from "https://cdn.jsdelivr.net/gh/jscroot/element@0.1.2/croot.js";
-
-export function main(){
-    setInner("biggreet","Halo Ngadimin");
-}
--- dc/rooted/jscroot/view/content/profile/info.js
+++
@@ -1,3 +0,0 @@
-export async function main(){
-    console.log("sudah di load info profile js");
-}
