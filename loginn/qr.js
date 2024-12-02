131db10a82a28519a8e7d63b3e27263abd7e3911
sipatir08 <fathir080604@gmail.com>
2024-12-03 01:05:32 +0700

    update

--- loginn/qr.js
+++ loginn/qr.js
@@ -2,8 +2,8 @@ import {qrController,deleteCookie} from "https://cdn.jsdelivr.net/gh/whatsauth/j
 import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/config.js";
    
 wauthparam.auth_ws="d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
-wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg1NjI0NDgyNTIxP3RleHQ9d2g0dDVhdXRoMA==";
+wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg1OTIyMDI0MDA3P3RleHQ9d2g0dDVhdXRoMA==";
 wauthparam.tokencookiehourslifetime=18;
-wauthparam.redirect ="/auth"
+wauthparam.redirect ="../product.html";
 deleteCookie(wauthparam.tokencookiename);
 qrController(wauthparam);
